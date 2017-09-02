43bf702e7c3d1ff6505548084526d7f16af4d668
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-02 13:54:46 -0300

    Implementa template strings no DateHelper + Valida se formato da data é correto para método

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,12 +5,12 @@ class DateHelper {
 	}
 
 	static stringToDate(string) {
+		if(!/\d{4}-\d{2}-\d{2}/.test(string))
+			throw new Error("Formato inválido. Correto: yyyy-mm-dd");
 		return new Date(string.split('-'));
 	}
 
 	static dateToString(date) {
-		return date.getDate() 
-			+ '/' + (date.getMonth() + 1)
-			+ '/' + date.getFullYear();
+		return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 	}
 }
