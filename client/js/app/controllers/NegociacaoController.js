64d7b1b4110913265e0efa2f3f536ed83daa9541
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-02 15:15:41 -0300

    Fixa controle dos elementos no controller + adiciona limpa formulário e melhora implementação no controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,19 +1,33 @@
 class NegociacaoController {
 
 	constructor() {
-		let $query = document.querySelector.bind(document);
+		var $query = document.querySelector.bind(document);
 			
-		this._inputData = $query('#data').value,
-		this._inputQuantidade = $query('#quantidade').value,
-		this._inputValor = $query('#valor').value;		
+		this._inputData = $query('#data'),
+		this._inputQuantidade = $query('#quantidade'),
+		this._inputValor = $query('#valor');
+		this._ListaNegociacoes = new ListaNegociacoes();
 	}
 
 	adiciona(event) {
 		event.preventDefault();
 
-		let negociacao = new Negociacao(DateHelper.stringToDate(this._inputData), this._inputQuantidade, this._inputValor);
-		console.log(negociacao);
-		console.log(DateHelper.dateToString(negociacao.data));
+		this._ListaNegociacoes.adiciona(this._criaNegociacao());
+		this._limpaFormulário();
+		console.log(this._ListaNegociacoes);
+
+	}
+
+	_limpaFormulário() {
+		this._inputData.value = "";
+		this._inputValor.value = "0";
+		this._inputQuantidade.value = 1;
+		this._inputData.focus();
+	}
+
+	_criaNegociacao() {
+
+		return new Negociacao(DateHelper.stringToDate(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
 
 	}
 
