4689d4890a6cdabd30e94988d6b81ad6012678e3
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-02 15:21:41 -0300

    Aprimora expressão regular na manipulação das datas

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,7 +5,7 @@ class DateHelper {
 	}
 
 	static stringToDate(string) {
-		if(!/\d{4}-\d{2}-\d{2}/.test(string))
+		if(!/^\d{4}-\d{2}-\d{2}$/.test(string))
 			throw new Error("Formato inválido. Correto: yyyy-mm-dd");
 		return new Date(string.split('-'));
 	}
