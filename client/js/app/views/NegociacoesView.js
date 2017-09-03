93aed481d731edb60f235f1a3daef044fb0378cf
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-03 13:24:49 -0300

    Aplicando arrow functions no retorno das negociacoes

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -18,28 +18,20 @@ class NegociacoesView {
 				</thead>
 
 				<tbody>
-					${model.negociacoes.map(n => {
-						return `
-							<tr>
-								<td>${DateHelper.dateToString(n.data)}</td>
-								<td>${n.quantidade}</td>
-								<td>${n.valor}</td>
-								<td>${n.volume}</td>
-							</tr>
-						`
-					}).join('')}
+					${model.negociacoes.map(n =>
+					`<tr>
+						<td>${DateHelper.dateToString(n.data)}</td>
+						<td>${n.quantidade}</td>
+						<td>${n.valor}</td>
+						<td>${n.volume}</td>
+					</tr>
+					`).join('')}
 				</tbody>
 
 				<tfoot>
 					<tr>
 						<td colspan=3></td>
-						<td>
-						${
-							model.negociacoes.reduce(function(total, n) {
-							return total += n.volume;
-							},0.0)
-						}
-						</td>
+						<td> ${model.negociacoes.reduce((total, n) => total += n.volume, 0.0)} </td>
 					</tr>
 				</tfoot>
 			</table>`;
