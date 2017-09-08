7368ce2496470541e839dae1e307ae97bd582ad0
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-08 15:47:34 -0300

    Implementa recurso para obrigar o dev a implementar determinado método quando usar uma classe que herda tais métodos

--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -4,8 +4,12 @@ class View {
 		this._elemento = elemento;
 	}
 
+	template() {
+		throw new Error('O método template deve ser implementado em sua view');
+	}
+
 	update(model){
-		this._elemento.innerHTML = this._template(model);
+		this._elemento.innerHTML = this.template(model);
 	}
 
 }
