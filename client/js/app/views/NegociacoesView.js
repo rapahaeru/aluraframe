eb73db5b1342b12192cf5305f5d11dc98587a565
Raphael Oliveira <rapahaeru@gmail.com>
2017-09-08 15:23:25 -0300

    Adiciona função com herança para reduzir número de código das views

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
 	constructor(elemento) {
-		this._elemento = elemento;
+		super(elemento);
 	}
 
 	_template(model) {
@@ -37,7 +37,4 @@ class NegociacoesView {
 			</table>`;
 	}
 
-	update(model) {
-		this._elemento.innerHTML = this._template(model);
-	}
 }
